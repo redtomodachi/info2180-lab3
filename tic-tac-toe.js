30756b6e88c9f68d66bec47d073d2a3b16efa8e7
Kemar Wilson <sonic3554@gmail.com>
2023-10-22 18:46:34 -0500

    Edited javascript files

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,10 +4,27 @@ document.addEventListener('DOMContentLoaded',function()
     if (board)
     {
         const squares = board.getElementsByTagName('div');
+        const game = new Array(squares.length).fill('E');
+        var currentMove = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
         for (let i = 0; i < squares.length;i++)
         {
             squares[i].classList.add('square');
-            squares[i].classList.add('empty');
+            squares[i].addEventListener('click',function()
+            {
+                if(currentMove == 2)
+                {
+                    game[i] = 'X';
+                    squares[i].innerHTML ='X'
+                    squares[i].classList.add('X');
+                    currentMove =1;
+                }else
+                {
+                    game[i] = 'O';
+                    squares[i].innerHTML = 'O';
+                    squares[i].classList.add('O');
+                    currentMove =2;
+                }
+            });
         }
 
     }
